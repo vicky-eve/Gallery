960ab0af6de5958f7c78047f48e2ac970f49cc58
vicky <victoria.awuor@student.moringaschool.com>
2022-03-25 23:25:09 +0300

    installed cloudinary and added the photo variable to image model classd

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-class User(models.Model):
+class Image(models.Model):
+    photo=CloudinaryField('Image')
     name=models.CharField(max_length=100)
     details=models.TextField(max_length=1000)
+    
+    def __str__(self):
+        return self.first_name
