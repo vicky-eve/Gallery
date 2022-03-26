86f5b59a91fc8693243bb8b73f971be771ceca0b
vicky <victoria.awuor@student.moringaschool.com>
2022-03-27 00:16:50 +0300

    update, save and delete tests for classes location and category

--- gallery/tests.py
+++ gallery/tests.py
@@ -50,5 +50,53 @@ class ImageTestClass(TestCase):
         location_search=self.image.filter_by_location('Roysambu')
         self.assertTrue(len(location_search)==1)
 
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.location=Location(name='Roysambu')
+        self.location.save_location
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location)>0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location)==0)
+
+    def test_update_location(self):
+        new_location='Juja'
+        self.location.update_location(self.location.id,new_location)
+        new_location=Image.objects.filter(location='Juja')
+        self.assertTrue(len(new_location)==1)
     
+class CategoryTestClass(TestCase):
+
+    def setUp(self):
+        self.category=Category(name='House facilities')
+        self.category.save_category
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category)==0)
+
+    def test_update_category(self):
+        new_category='Bags'
+        self.category.update_category(self.category.id,new_category)
+        new_category=Image.objects.filter(photo='Bags')
+        self.assertTrue(len(new_category)==1)
 
