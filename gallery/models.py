ee36ee49d5daaa3d81b81bef561e654dcda7ab39
vicky <victoria.awuor@student.moringaschool.com>
2022-03-26 08:02:25 +0300

    created the category and location models

--- gallery/models.py
+++ gallery/models.py
@@ -4,10 +4,16 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    image=models.ImageField(upload_to=('images/'))
+    photo=models.ImageField(upload_to=('images/'))
     date_posted=models.DateTimeField(auto_now_add=True)
     name=models.CharField(max_length=100)
     details=models.TextField(max_length=1000)
     
     def save_photo(self):
         self.save()
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+class Location(models.Model):
+    name=models.CharField(max_length=100)
--- mygallery/settings.py
+++ mygallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -131,3 +133,5 @@ DATABASES = {
     'PASSWORD':'aderazi',
     }
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
