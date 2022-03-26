933652564b526a3f3624eec1dbfdf0dbf4aeb578
vicky <victoria.awuor@student.moringaschool.com>
2022-03-26 22:58:47 +0300

    update image test for image class

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,9 +2,6 @@ from django.test import TestCase
 from .models import Image, Location, Category
 
 # Create your tests here.
-
-        
-
 class ImageTestClass(TestCase):
     def setUp(self):
         """ 
@@ -18,6 +15,27 @@ class ImageTestClass(TestCase):
 
         self.image = Image(name= 'Wardrobe', photo= 'wardrobe.png', details= 'the beauty of a bedroom is in the location and design of the wardrobe', location=self.location, category=self.category)
         self.image.save_image
+
     def test_instance(self):
         self.assertTrue(isinstance(self.image,Image))
+
+   
+
+    def test_save_image(self):
+        self.image.save_image
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+    
+    def test_delete_image(self):
+        image=Image.objects.filter(name='imagetest')
+        image.delete()
+        self.assertTrue(len(Image.objects.all())==0)
+
+    def test_update_image(self):
+        new_photo='Wallunit.png'
+        self.image.update_image(self.image.id,new_photo)
+        new_photo=Image.objects.filter(photo='Wallunit.png')
+        self.assertTrue(len(new_photo)==1)
+    
+    
 
