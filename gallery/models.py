f66616092bd7474e717eba80dbd0eac0f6abe55b
vicky <victoria.awuor@student.moringaschool.com>
2022-03-26 08:05:15 +0300

    created the relation between class image and category and locatuion

--- gallery/models.py
+++ gallery/models.py
@@ -3,17 +3,19 @@ from django.db import models
 
 # Create your models here.
 
-class Image(models.Model):
-    photo=models.ImageField(upload_to=('images/'))
-    date_posted=models.DateTimeField(auto_now_add=True)
-    name=models.CharField(max_length=100)
-    details=models.TextField(max_length=1000)
-    
-    def save_photo(self):
-        self.save()
-
 class Category(models.Model):
     name=models.CharField(max_length=100)
 
 class Location(models.Model):
     name=models.CharField(max_length=100)
+
+class Image(models.Model):
+    photo=models.ImageField(upload_to=('images/'))
+    date_posted=models.DateTimeField(auto_now_add=True)
+    name=models.CharField(max_length=100)
+    details=models.TextField(max_length=1000)
+    category=models.ForeignKey(Category)
+    location=models.ForeignKey(Location)
+    
+    def save_photo(self):
+        self.save()
