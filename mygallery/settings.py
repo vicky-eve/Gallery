fd256183746969160104c837a181d59f169a131c
vicky <victoria.awuor@student.moringaschool.com>
2022-03-29 12:25:16 +0300

    configuration

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -65,7 +65,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-b(w&p&*pzt1=*5naxg^2qw3_ln1)yqo#b7jdol6825^bltwvan'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
